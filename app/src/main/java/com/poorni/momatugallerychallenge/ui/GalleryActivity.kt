681b43aa6e38b1d8a11db8d0f74b2c13f2c5f798
poornima <poornima@agunity>
2020-11-30 20:21:24 +1100

    Initial Commit

---
+++ app/src/main/java/com/poorni/momatugallerychallenge/ui/GalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.poorni.momatugallerychallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.poorni.momatugallerychallenge.R
+
+class GalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
